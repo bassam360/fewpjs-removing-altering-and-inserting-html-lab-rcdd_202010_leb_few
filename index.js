00750ef25ab12ee616bc7c9c7306664426a86f49
bassam edelbi <bassamedelbi360@gmail.com>
2020-12-29 18:22:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.querySelector("main#main").remove();
-const="sam"
+
 const newHeader = document.createElement("h1");
-newHeader.id = "me"
-newHeader.innerHTML = "meee";
-newHeader.className = "meeeee";
+newHeader.id = "victory"
+newHeader.innerHTML = "";
+newHeader.className = "victory";
